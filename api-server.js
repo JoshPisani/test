3ed4ce15ff722b7905a6ccf12ccaa43dc96e65d5
JoshPisani <joshuapisani92@gmail.com>
2021-06-20 22:57:26 +1000

    2 files modified

--- api-server.js
+++ api-server.js
@@ -20,6 +20,12 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
